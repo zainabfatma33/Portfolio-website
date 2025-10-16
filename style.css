964e4874afc8e9456087f482933f63c8a0197768
Zainab Fatma <zainabfatma369@gmail.com>
2025-10-16 21:18:30 +0530

    Add smooth scroll functionality for navigation links

---
+++ style.css
@@ -0,0 +1,9 @@
+// Smooth scroll for nav links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
